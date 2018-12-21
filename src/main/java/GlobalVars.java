c8ca63c479e1abff053ec0b01e9e8f1220020680
AndriiKulchytskyi <resprod90@gmail.com>
2018-12-21 18:06:30 +0200

    Groups of people added.

--- src/main/java/GlobalVars.java
+++ src/main/java/GlobalVars.java
@@ -2,4 +2,6 @@ import java.util.HashMap;
 
 public class GlobalVars {
     public static HashMap<Integer,Integer> usersWithNumbers=new HashMap<Integer,Integer>();
+    public static HashMap<String,Integer> usersNamesWithNumbers=new HashMap<String,Integer>();
+
 }
--- src/main/java/MessageHandler.java
+++ src/main/java/MessageHandler.java
@@ -1,20 +1,35 @@
 
 public class MessageHandler {
 
-    public static String handle(Integer id,String name) {
+    public static String handle(Integer id, String name) {
         //TODO Which group.
-        int min=0;
-        int max=9;
-        int i = (int) (Math.random() * ( max - min ));
-        GlobalVars.usersWithNumbers.put(id,i);
-        return "You receive number " + i;
+        int min = 1;
+        int max = 4;
+        int number = 0;
+        boolean numberIsNotFound = true;
+        while (numberIsNotFound) {
+            number = (int) ((Math.random() * (max - min)))+1;
+            if (!GlobalVars.usersWithNumbers.containsValue(number)) {
+                numberIsNotFound = false;
+            }
+        }
+        GlobalVars.usersWithNumbers.put(id, number);
+        GlobalVars.usersNamesWithNumbers.put(name, number);
+        return "You receive number " + number;
     }
 
-    public static String handleTypicalAnswer(Integer id,String name) {
-        return name+", you already received number " + GlobalVars.usersWithNumbers.get(id) + ". You cannot change it.\nAsk for /help";
+    public static String handleTypicalAnswer(Integer id, String name) {
+        StringBuilder names = new StringBuilder();
+        for (String key:GlobalVars.usersNamesWithNumbers.keySet()){
+            names.append(key+",");
+        }
+        names.append("\n");
+        return name + ", your number " + GlobalVars.usersWithNumbers.get(id) + "\nIn your group:"
+                +names
+                +"You cannot change it.\nAsk for /help";
     }
 
-    public static String handleHelp(){
+    public static String handleHelp() {
         return "That bot was created in very narrow aims of group of people\nDate of birth: 10.12.18 ";
     }
 }
